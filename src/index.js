d1fadd25372fd50ca4b6d7b669383211e8876fb9
huynhtehoa <huynhtehoa@gmail.com>
2020-01-11 16:28:13 +0700

    refactor index

--- src/index.js
+++ src/index.js
@@ -16,88 +16,166 @@ import ChangePw from "views/Sections/ChangePw.jsx";
 import NotFound from "views/Sections/NotFound.jsx";
 import ContactUs from "views/ContactUs/ContactUs.jsx";
 import AddMore from "views/AddMorePage/AddMore.jsx";
-import EditRecipe from "views/EditPage/EditRecipe.jsx"
-import FavoritePage from "views/FavoritePage/FavoritePage.jsx"
+import EditRecipe from "views/EditPage/EditRecipe.jsx";
+import FavoritePage from "views/FavoritePage/FavoritePage.jsx";
 
 var hist = createBrowserHistory();
 
 const App = () => {
-
-  const [token, setToken] = useState('')
-  const [isLogin, setLogin] = useState(false)
-  const [name, setName] = useState('')
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [token, setToken] = useState("");
+  const [isLogin, setLogin] = useState(false);
+  const [name, setName] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     if (window.location.search.includes("?api_key=")) {
-      const accessToken = (window.location.search.split("=")[0] === "?api_key") ? window.location.search.split("=")[1] : null;
+      const accessToken =
+        window.location.search.split("=")[0] === "?api_key"
+          ? window.location.search.split("=")[1]
+          : null;
       if (accessToken) {
-        localStorage.setItem('token', accessToken.replace('?api_key=', ''))
-        setToken(accessToken)
-        setLogin(true)
+        localStorage.setItem("token", accessToken.replace("?api_key=", ""));
+        setToken(accessToken);
+        setLogin(true);
       }
-      const existingToken = localStorage.getItem('token');
+      const existingToken = localStorage.getItem("token");
       if (existingToken && existingToken !== "undefined") {
-        setToken(existingToken)
-        setLogin(true)
+        setToken(existingToken);
+        setLogin(true);
       }
-      getWithToken()
+      getWithToken();
     }
-  }, [])
+  }, []);
 
   const getWithToken = async () => {
     const url = `https://jftjf-backend.herokuapp.com/auth`;
     let response = await fetch(url, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-type': 'application/json',
-        'Authorization': `Token ${localStorage.getItem('token')}`,
+        "Content-type": "application/json",
+        Authorization: `Token ${localStorage.getItem("token")}`
       }
-    })
-    let jsonData = await response.json()
-    setName(jsonData.name)
-    setIsAdmin(jsonData.isAdmin)
-    setLogin(true)
-  }
+    });
+    try {
+      let jsonData = await response.json();
+      setName(jsonData.name);
+      setIsAdmin(jsonData.isAdmin);
+      setLogin(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const clearToken = () => {
-    clearBackendToken()
-    localStorage.clear("token")
-    setName('')
-    setIsAdmin(false)
-    setLogin(false)
-  }
+    clearBackendToken();
+    localStorage.clear("token");
+    setName("");
+    setIsAdmin(false);
+    setLogin(false);
+  };
 
   const clearBackendToken = async () => {
     const url = `https://jftjf-backend.herokuapp.com/logout`;
     await fetch(url, {
       headers: {
-        'Content-type': 'application/json',
-        'Authorization': `Token ${localStorage.getItem('token')}`,
+        "Content-type": "application/json",
+        Authorization: `Token ${localStorage.getItem("token")}`
       }
-    })
-  }
+    });
+  };
 
   return (
     <Router history={hist}>
       <Switch>
-        <Route exact path="/" component={() => <LandingPage isLogin={isLogin} clearToken={clearToken} name={name} token={token} isAdmin={isAdmin} />} />
+        <Route
+          exact
+          path="/"
+          component={() => (
+            <LandingPage
+              isLogin={isLogin}
+              clearToken={clearToken}
+              name={name}
+              token={token}
+              isAdmin={isAdmin}
+            />
+          )}
+        />
         <Route exact path="/replacement/:inputSearch" component={Replacement} />
         <Route exact path="/favorite/" component={FavoritePage} />
-        <Route exact path="/addmore" component={() => <AddMore isLogin={isLogin} clearToken={clearToken} name={name} token={token} isAdmin={isAdmin} />} />
-        <Route exact path="/register-page" component={() => <RegisterPage token={token} isAdmin={isAdmin} name={name} isLogin={isLogin} />} />
-        <Route exact path="/login-page" component={() => <LoginPage isLogin={isLogin} getWithToken={getWithToken} clearToken={clearToken} name={name} token={token} isAdmin={isAdmin} />} />
+        <Route
+          exact
+          path="/addmore"
+          component={() => (
+            <AddMore
+              isLogin={isLogin}
+              clearToken={clearToken}
+              name={name}
+              token={token}
+              isAdmin={isAdmin}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/register-page"
+          component={() => (
+            <RegisterPage
+              token={token}
+              isAdmin={isAdmin}
+              name={name}
+              isLogin={isLogin}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/login-page"
+          component={() => (
+            <LoginPage
+              isLogin={isLogin}
+              getWithToken={getWithToken}
+              clearToken={clearToken}
+              name={name}
+              token={token}
+              isAdmin={isAdmin}
+            />
+          )}
+        />
         <Route exact path="/forget-pw" component={ForgetPw} />
         <Route exact path="/reset-pw/" component={ResetPw} />
         <Route exact path="/change-pw/" component={ChangePw} />
-        <Route exact path="/discover" component={() => <DiscoverPage isLogin={isLogin} getWithToken={getWithToken} clearToken={clearToken} name={name} token={token} isAdmin={isAdmin} />} />
+        <Route
+          exact
+          path="/discover"
+          component={() => (
+            <DiscoverPage
+              isLogin={isLogin}
+              getWithToken={getWithToken}
+              clearToken={clearToken}
+              name={name}
+              token={token}
+              isAdmin={isAdmin}
+            />
+          )}
+        />
         <Route exact path="/edit/recipe/:dataId" component={EditRecipe} />
-        <Route exact path="/contactus" component={() => <ContactUs isLogin={isLogin} clearToken={clearToken} name={name} token={token} isAdmin={isAdmin} />} />
+        <Route
+          exact
+          path="/contactus"
+          component={() => (
+            <ContactUs
+              isLogin={isLogin}
+              clearToken={clearToken}
+              name={name}
+              token={token}
+              isAdmin={isAdmin}
+            />
+          )}
+        />
         <Route component={NotFound} />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById("root"))
-
+ReactDOM.render(<App />, document.getElementById("root"));
